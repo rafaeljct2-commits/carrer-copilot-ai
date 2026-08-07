793e8d730e66a8a843ca6922a6bd2927940b84ee
Rafael Pereira de Souza <rafaeljct2@gmail.com>
2026-08-07 09:01:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+import streamlit as st
+from utilitarios.pdf_reader import extract_text
 
+st.set_page_config(
+    page_title="Career Copilot AI",
+    page_icon="🚀"
+)
+
+st.title("🚀 Career Copilot AI")
+
+uploaded_file = st.file_uploader(
+    "Upload Resume PDF",
+    type=["pdf"]
+)
+
+if uploaded_file:
+
+    text = extract_text(uploaded_file)
+
+    st.success("Resume uploaded successfully!")
+
+    st.subheader("Resume Content")
+
+    st.text_area(
+        "Extracted Text",
+        text,
+        height=300
+    )
