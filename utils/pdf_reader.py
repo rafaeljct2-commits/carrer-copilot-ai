c1f2b310435c4830c87567b91e1323add5fa4c2f
Rafael Pereira de Souza <rafaeljct2@gmail.com>
2026-08-07 08:58:55 -0300

    Update pdf_reader.py

--- utils/pdf_reader.py
+++ utils/pdf_reader.py
@@ -1,1 +1,15 @@
 
+from pypdf import PdfReader
+
+def extract_text(pdf_file):
+    reader = PdfReader(pdf_file)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text
+
+    return text
