680183dcb8d3af6dcd7d31147f40074b54d44ce2
Rafael Pereira de Souza <rafaeljct2@gmail.com>
2026-08-07 22:38:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from utils.pdf_reader import extract_text
 from utils.skill_analyzer import analyze_skills
+from utils.language_detector import detect_language
 
 st.set_page_config(page_title="Career Copilot AI")
 
@@ -15,32 +16,39 @@ uploaded_file = st.file_uploader(
 if uploaded_file:
     text = extract_text(uploaded_file)
 
+    language = detect_language(text)
+
+    st.subheader("Detected Language")
+    st.success(language)
+
     st.subheader("Extracted Resume Text")
     st.write(text)
 
     skills = analyze_skills(text)
 
-    st.subheader("Skills Identificadas")
+    st.subheader("Skills Identified")
 
     if skills:
         for skill in skills:
             st.success(skill)
     else:
-        st.warning("Nenhuma skill encontrada.")
+        st.warning("No skills found.")
 
-    st.subheader("Recomendações")
+    st.subheader("Recommendations")
 
     recommendations = []
 
-    if "python" not in skills:
-        recommendations.append("Aprender Python")
+    if "Python" not in skills:
+        recommendations.append("Learn Python")
 
-    if "sql" not in skills:
-        recommendations.append("Aprender SQL")
+    if "SQL" not in skills:
+        recommendations.append("Learn SQL")
 
-    if "power bi" not in skills:
-        recommendations.append("Aprender Power BI")
+    if "Power BI" not in skills:
+        recommendations.append("Learn Power BI")
+
+    if "English" not in skills:
+        recommendations.append("Improve English")
 
     for recommendation in recommendations:
         st.info(recommendation)
- 
